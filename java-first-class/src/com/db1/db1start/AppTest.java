7654f449ff28a29b512ca51b2cce1bf6df488555
Leonardo Vendrame <leonvendrame@gmail.com>
2019-11-13 14:14:48 -0300

    Java básico: exercícios de aula Exercícios básicos de integers, doubles e strings Testes unitários adicionados para os métodos

--- java-first-class/src/com/db1/db1start/AppTest.java
+++ java-first-class/src/com/db1/db1start/AppTest.java
@@ -22,66 +22,66 @@ public class AppTest {
     }
 
     @Test
-    public void subExpect0() {
+    public void subtractExpect0() {
         Application application = new Application();
         Integer expectedResult = 0;
-        Integer response = application.sub(9, 9);
+        Integer response = application.subtract(9, 9);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void sumExpectMinus1() {
+    public void subtractExpectMinus1() {
         Application application = new Application();
         Integer expectedResult = -1;
-        Integer response = application.sub(1, 2);
+        Integer response = application.subtract(1, 2);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void divExpect0() {
+    public void divideExpect0() {
         Application application = new Application();
         Integer expectedResult = 0;
-        Integer response = application.div(9, 0);
+        Integer response = application.divide(9, 0);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void divExpect2() {
+    public void divideExpect2() {
         Application application = new Application();
         Integer expectedResult = 2;
-        Integer response = application.div(4, 2);
+        Integer response = application.divide(4, 2);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void mulExpect36() {
+    public void multiplyExpect36() {
         Application application = new Application();
         Integer expectedResult = 36;
-        Integer response = application.mul(6, 6);
+        Integer response = application.multiply(6, 6);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void mulExpect0() {
+    public void multiplyExpect0() {
         Application application = new Application();
         Integer expectedResult = 0;
-        Integer response = application.mul(0, 4);
+        Integer response = application.multiply(0, 4);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void mulExpectMinus5() {
+    public void multiplyExpectMinus5() {
         Application application = new Application();
         Integer expectedResult = -5;
-        Integer response = application.mul(5, -1);
+        Integer response = application.multiply(5, -1);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void divExpect1() {
+    public void divideExpect1() {
         Application application = new Application();
         Integer expectedResult = 1;
-        Integer response = application.div(3, 3);
+        Integer response = application.divide(3, 3);
         Assert.assertEquals(expectedResult, response);
     }
 
@@ -126,20 +126,139 @@ public class AppTest {
     }
 
     @Test
-    public void howMuchOddsToExpect5() {
+    public void howManyOddsToExpect5() {
         Application application = new Application();
         Integer expectedResult = 5;
-        Integer response = application.howMuchOddsTo(10);
+        Integer response = application.howManyOddsTo(10);
         Assert.assertEquals(expectedResult, response);
     }
 
     @Test
-    public void howMuchOddsToExpect2() {
+    public void howManyOddsToExpect2() {
         Application application = new Application();
         Integer expectedResult = 2;
-        Integer response = application.howMuchOddsTo(3);
+        Integer response = application.howManyOddsTo(3);
         Assert.assertEquals(expectedResult, response);
     }
 
+    @Test
+    public void vowelCountExpect5() {
+        Application application = new Application();
+        Integer expectedResult = 5;
+        Integer response = application.vowelCount("a e iou");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void vowelCountExpect0() {
+        Application application = new Application();
+        Integer expectedResult = 0;
+        Integer response = application.vowelCount("bcdfghjklmnpqrstvwxyz ");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void toUpperCaseExpectABC() {
+        Application application = new Application();
+        String expectedResult = "ABC";
+        String response = application.toUpperCase("abc");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void toLowerCaseExpectAbc() {
+        Application application = new Application();
+        String expectedResult = "abc";
+        String response = application.toLowerCase("ABC");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void first4LettersExpectMari() {
+        Application application = new Application();
+        String expectedResult = "Mari";
+        String response = application.first4Letters("Maria Lobos");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void from3LetterExpectOnardo() {
+        Application application = new Application();
+        String expectedResult = "onardo Vendrame";
+        String response = application.from3Letter("Leonardo Vendrame");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void last4LettersExpectIana() {
+        Application application = new Application();
+        String expectedResult = "iana";
+        String response = application.last4Letters("Mariana Lália");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void changeFirstNameExpectAlunoa() {
+        Application application = new Application();
+        String expectedResult = "Aluno(a) da Silva";
+        String response = application.changeFirstName("João da Silva");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void commaSpaceSplitExpectBananaMacaMelancia() {
+        Application application = new Application();
+        String[] expectedResult = {"banana", "maçã", "melância"};
+        String[] response = application.commaSpaceSplit("banana, maçã, melância");
+        Assert.assertArrayEquals(expectedResult, response);
+    }
+
+    @Test
+    public void reverseStringExpectCba() {
+        Application application = new Application();
+        String expectedResult = "cba";
+        String response = application.reverseString("abc");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void howManyLettersExpect7() {
+        Application application = new Application();
+        Integer expectedResult = 7;
+        Integer response = application.howManyLetters("  DB1 START  ");
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void simpleAverageExpect10() {
+        Application application = new Application();
+        Double expectedResult = 10.0;
+        Double response = application.simpleAverage(10.0, 20.0, 0.0);
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void minDoubleExpectMinus3() {
+        Application application = new Application();
+        Double expectedResult = -3.0;
+        Double response = application.minDouble(-3.0, 0.0, 2.0);
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void minDoubleExpect0() {
+        Application application = new Application();
+        Double expectedResult = 0.0;
+        Double response = application.minDouble(0.0, 2.0, 3.0);
+        Assert.assertEquals(expectedResult, response);
+    }
+
+    @Test
+    public void areaOfTriangleExpect4() {
+        Application application = new Application();
+        Double expectedResult = 4.0;
+        Double response = application.areaOfTriangle(2.0,4.0);
+        Assert.assertEquals(expectedResult, response);
+    }
 
 }
--- java-first-class/src/com/db1/db1start/Application.java
+++ java-first-class/src/com/db1/db1start/Application.java
@@ -6,15 +6,15 @@ public class Application {
         return (number1 + number2);
     }
 
-    public Integer sub(Integer number1, Integer number2) {
+    public Integer subtract(Integer number1, Integer number2) {
         return (number1 - number2);
     }
 
-    public Integer mul(Integer number1, Integer number2) {
+    public Integer multiply(Integer number1, Integer number2) {
         return (number1 * number2);
     }
 
-    public Integer div(Integer dividend, Integer divisor) {
+    public Integer divide(Integer dividend, Integer divisor) {
         if (divisor != 0) {
             return (dividend / divisor);
         }
@@ -22,10 +22,7 @@ public class Application {
     }
 
     public boolean isEven(Integer number) {
-        if (number % 2 == 0) {
-            return true;
-        }
-        return false;
+        return (number % 2 == 0);
     }
 
     public Integer max(Integer number1, Integer number2) {
@@ -35,12 +32,77 @@ public class Application {
         return number2;
     }
 
-    public Integer howMuchOddsTo(Integer number) {
-        Integer response = number / 2;
-        if (!this.isEven(number)) {
-            return (response + 1);
+    public Integer howManyOddsTo(Integer number) {
+        return ((number / 2) + (number % 2));
+    }
+
+    public String toUpperCase(String string) {
+        return string.toUpperCase();
+    }
+
+    public String toLowerCase(String string) {
+        return string.toLowerCase();
+    }
+
+    public Integer howManyCharacters(String string) {
+        return string.replaceAll("[^a-zA-Z]","").length();
+    }
+
+    public Integer howManyLetters(String string) {
+        return  string.trim().replaceAll("[^a-zA-Z]", "").length();
+    }
+
+    public String first4Letters(String name) {
+        return name.substring(0, 4);
+    }
+
+    public String from3Letter(String name) {
+        return name.substring(2);
+    }
+
+    public String last4Letters(String name) {
+        String nameOnly = name.split(" ")[0];
+        return nameOnly.substring(nameOnly.length() - 4);
+    }
+
+    public String changeFirstName(String name) {
+        String[] separatedNames = name.split(" ");
+        String response = "";
+        separatedNames[0] = "Aluno(a)";
+        for (int i = 0; i < separatedNames.length; ++i) {
+            response += separatedNames[i];
+            if (i != separatedNames.length - 1) {
+                response += " ";
+            }
         }
         return response;
     }
 
+    public String[] commaSpaceSplit(String string) {
+        return string.split(", ");
+    }
+
+    public Integer vowelCount(String string) {
+        return string.trim().replaceAll("[^aeiou]", "").length();
+    }
+
+    public String reverseString(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    public Double minDouble(Double number1, Double number2) {
+        return (number1 < number2 ? number1 : number2);
+    }
+
+    public Double minDouble(Double number1, Double number2, Double number3) {
+        return (minDouble(minDouble(number1, number2), number3));
+    }
+
+    public Double simpleAverage(Double number1, Double number2, Double number3) {
+        return ((number1 + number2 + number3) / 3);
+    }
+
+    public Double areaOfTriangle(Double base, Double height) {
+        return ((base * height) / 2);
+    }
 }
